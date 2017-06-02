227e39ea8e2d00ab6e5e5185b625b33b8f79f35d
Han Xiao <hxiao@anthemengineering.com>
2017-06-02 10:52:19 -0400

    Complete Basic FF1 Implementation
    Complete basic FF1 implementation to pass all provided sample test
    cases and being basic code cleanup for implementation and test
    functions.

--- fpe.go
+++ fpe.go
@@ -3,7 +3,6 @@ package fpe
 import (
 	"crypto/cipher"
 	"encoding/binary"
-	"fmt"
 	"math"
 	"math/big"
 	"strconv"
@@ -27,7 +26,7 @@ type FF1 struct {
 	fixedBlock               [16]byte
 }
 
-func NewFF1(cipher cipher.Block, radix int, minMessageLength int, maxMessageLength int, maxTweakLength int) (ff1 FF1) {
+func NewFF1(cipher cipher.Block, radix, minMessageLength, maxMessageLength, maxTweakLength int) (ff1 FF1) {
 	return FF1{
 		cipher:           cipher,
 		radix:            radix,
@@ -36,7 +35,7 @@ func NewFF1(cipher cipher.Block, radix int, minMessageLength int, maxMessageLeng
 		maxTweakLength:   maxTweakLength}
 }
 
-func (ff1 *FF1) setup(message string) {
+func (ff1 *FF1) setup(message string, tweak []byte) {
 	ff1.messageLength = len(message)
 	ff1.firstHalfLength = int(math.Floor(float64(ff1.messageLength) / 2.0))
 	ff1.secondHalfLength = ff1.messageLength - ff1.firstHalfLength
@@ -45,12 +44,12 @@ func (ff1 *FF1) setup(message string) {
 	ff1.adjustedByteStringLength = int(4*math.Ceil(float64(ff1.messageByteLength)/4.0) + 4)
 
 	fixedBlockPart1 := uint64(0x0102010000000a00) | (uint64(ff1.radix) << 16) | uint64(ff1.firstHalfLength%256)
-	fixedBlockPart2 := (uint64(ff1.messageLength) << 32) | uint64(ff1.maxTweakLength)
+	fixedBlockPart2 := (uint64(ff1.messageLength) << 32) | uint64(len(tweak))
 	binary.BigEndian.PutUint64(ff1.fixedBlock[:8], fixedBlockPart1)
 	binary.BigEndian.PutUint64(ff1.fixedBlock[8:], fixedBlockPart2)
 }
 
-func (ff1 *FF1) PseudoRandomFunction(blockString []byte) (block []byte) {
+func (ff1 *FF1) pseudoRandomFunction(blockString []byte) (block []byte) {
 	numBlocks := len(blockString) / 16
 	block = make([]byte, 16)
 	for index := 0; index < numBlocks; index++ {
@@ -60,26 +59,28 @@ func (ff1 *FF1) PseudoRandomFunction(blockString []byte) (block []byte) {
 	return block
 }
 
-func (ff1 *FF1) Encrypt(plaintext string, tweak []byte) (message string, err error) {
-	ff1.setup(plaintext)
+func (ff1 *FF1) calculateAdjustedByteStringNumber(block []byte) (adjustedByteStringNumber *big.Int) {
+	byteString := make([]byte, int(math.Ceil(float64(ff1.adjustedByteStringLength)/16.0))*16)
+	copy(byteString[0:16], block)
+	mask := make([]byte, 2)
+	for blockIndex := 1; blockIndex*16 < len(block); blockIndex++ {
+		binary.BigEndian.PutUint16(mask, uint16(blockIndex))
+		ff1.cipher.Encrypt(byteString[blockIndex*16:(blockIndex+1)*16], xorBytes(block, mask))
+	}
+	adjustedByteString := byteString[0:ff1.adjustedByteStringLength]
+	adjustedByteStringNumber = big.NewInt(0)
+	adjustedByteStringNumber.SetBytes(adjustedByteString)
+	return adjustedByteStringNumber
+}
 
-	fmt.Println("Message Length (n): ", ff1.messageLength)
-	fmt.Println("First Half Length (u): ", ff1.firstHalfLength)
-	fmt.Println("Second Half Length (v): ", ff1.secondHalfLength)
-	fmt.Println("First Half (A): ", ff1.firstHalf)
-	fmt.Println("Second Half (B): ", ff1.secondHalf)
-	fmt.Println("Byte Length (b): ", ff1.messageByteLength)
-	fmt.Println("Adjusted Byte Length (d): ", ff1.adjustedByteStringLength)
-	fmt.Println("Fixed Block (P): ", ff1.fixedBlock)
-	fmt.Println()
+func (ff1 *FF1) Encrypt(plaintext string, tweak []byte) (message string, err error) {
+	ff1.setup(plaintext, tweak)
 
 	variableBlockLength := len(tweak) + 1 + ff1.messageByteLength
 	variableBlockLength = variableBlockLength + (16 - variableBlockLength%16) //round variable block length to next multiple of 16 bytes (128 bits)
 	variableBlock := make([]byte, variableBlockLength)
 	copy(variableBlock, tweak)
 	for round := 0; round < 10; round++ {
-		fmt.Println("Round #", round, ":")
-
 		variableBlock[variableBlockLength-ff1.messageByteLength-1] = byte(round)
 		secondHalfNumber, err := strconv.ParseUint(ff1.secondHalf, ff1.radix, 64)
 		if err != nil {
@@ -88,25 +89,13 @@ func (ff1 *FF1) Encrypt(plaintext string, tweak []byte) (message string, err err
 		tmpBuf := make([]byte, 8)
 		binary.BigEndian.PutUint64(tmpBuf, secondHalfNumber)
 		copy(variableBlock[variableBlockLength-ff1.messageByteLength:], tmpBuf[8-ff1.messageByteLength:])
-		blockString := make([]byte, 32)
-		copy(blockString, ff1.fixedBlock[:])
-		copy(blockString[16:], variableBlock)
-		block := ff1.PseudoRandomFunction(append(ff1.fixedBlock[:], variableBlock...))
+		block := ff1.pseudoRandomFunction(append(ff1.fixedBlock[:], variableBlock...))
 
-		byteString := make([]byte, int(math.Ceil(float64(ff1.adjustedByteStringLength)/16.0))*16)
-		copy(byteString[0:16], block)
-		mask := make([]byte, 2)
-		for blockIndex := 1; blockIndex*16 < len(block); blockIndex++ {
-			binary.BigEndian.PutUint16(mask, uint16(blockIndex))
-			ff1.cipher.Encrypt(byteString[blockIndex*16:(blockIndex+1)*16], xorBytes(block, mask))
-		}
-		adjustedByteString := byteString[0:ff1.adjustedByteStringLength]
-		adjustedByteStringNumber := big.NewInt(0)
-		adjustedByteStringNumber.SetBytes(adjustedByteString)
+		adjustedByteStringNumber := ff1.calculateAdjustedByteStringNumber(block)
 
-		modifier := ff1.secondHalfLength
+		resultStringLength := ff1.secondHalfLength
 		if round%2 == 0 {
-			modifier = ff1.firstHalfLength
+			resultStringLength = ff1.firstHalfLength
 		}
 
 		firstHalfNumber, err := strconv.ParseInt(ff1.firstHalf, ff1.radix, 64)
@@ -114,59 +103,26 @@ func (ff1 *FF1) Encrypt(plaintext string, tweak []byte) (message string, err err
 			return message, err
 		}
 
-		resultingNumber := big.NewInt(firstHalfNumber)
-		resultingNumber.Add(big.NewInt(firstHalfNumber), adjustedByteStringNumber)
-		resultingNumber.Mod(resultingNumber, big.NewInt(int64(math.Pow(float64(ff1.radix), float64(modifier)))))
+		resultNumber := big.NewInt(0).Add(big.NewInt(firstHalfNumber), adjustedByteStringNumber)
+		resultNumber.Mod(resultNumber, big.NewInt(int64(math.Pow(float64(ff1.radix), float64(resultStringLength)))))
 
-		resultingString := strconv.FormatUint(resultingNumber.Uint64(), ff1.radix)
-		if len(resultingString) < modifier {
-			padding := strings.Repeat("0", modifier-len(resultingString))
-			resultingString = padding + resultingString
-		}
+		resultString := zeroLeftPad(strconv.FormatUint(resultNumber.Uint64(), ff1.radix), resultStringLength)
 		ff1.firstHalf = ff1.secondHalf
-		ff1.secondHalf = resultingString
-
-		fmt.Println("Variable Block (Q): ", variableBlock)
-		fmt.Println("Block (R): ", block)
-		fmt.Printf("Byte String (S): %x\n", adjustedByteString)
-		fmt.Println("Byte String as Number (y): ", adjustedByteStringNumber)
-		fmt.Println("Modifier (m): ", modifier)
-		fmt.Println("Resulting Number (c): ", resultingNumber)
-		fmt.Println("Resulting String (C): ", resultingString)
-		fmt.Println("New First Half(A): ", ff1.firstHalf)
-		fmt.Println("New Second Half(B): ", ff1.secondHalf)
-
-		fmt.Println()
+		ff1.secondHalf = resultString
 	}
 
 	message = ff1.firstHalf + ff1.secondHalf
-
-	fmt.Println("Encrypted Message (A || B): ", message)
-	fmt.Println()
-
 	return message, nil
 }
 
 func (ff1 *FF1) Decrypt(message string, tweak []byte) (plaintext string, err error) {
-	ff1.setup(message)
-
-	fmt.Println("Message Length (n): ", ff1.messageLength)
-	fmt.Println("First Half Length (u): ", ff1.firstHalfLength)
-	fmt.Println("Second Half Length (v): ", ff1.secondHalfLength)
-	fmt.Println("First Half (A): ", ff1.firstHalf)
-	fmt.Println("Second Half (B): ", ff1.secondHalf)
-	fmt.Println("Byte Length (b): ", ff1.messageByteLength)
-	fmt.Println("Adjusted Byte Length (d): ", ff1.adjustedByteStringLength)
-	fmt.Println("Fixed Block (P): ", ff1.fixedBlock)
-	fmt.Println()
+	ff1.setup(message, tweak)
 
 	variableBlockLength := len(tweak) + 1 + ff1.messageByteLength
 	variableBlockLength = variableBlockLength + (16 - variableBlockLength%16) //round variable block size to next multiple of 16
 	variableBlock := make([]byte, variableBlockLength)
 	copy(variableBlock, tweak)
 	for round := 9; round >= 0; round-- {
-		fmt.Println("Round #", round, ":")
-
 		variableBlock[variableBlockLength-ff1.messageByteLength-1] = byte(round)
 		firstHalfNumber, err := strconv.ParseUint(ff1.firstHalf, ff1.radix, 64)
 		if err != nil {
@@ -175,25 +131,13 @@ func (ff1 *FF1) Decrypt(message string, tweak []byte) (plaintext string, err err
 		tmpBuf := make([]byte, 8)
 		binary.BigEndian.PutUint64(tmpBuf, firstHalfNumber)
 		copy(variableBlock[variableBlockLength-ff1.messageByteLength:], tmpBuf[8-ff1.messageByteLength:])
-		blockString := make([]byte, 32)
-		copy(blockString, ff1.fixedBlock[:])
-		copy(blockString[16:], variableBlock)
-		block := ff1.PseudoRandomFunction(append(ff1.fixedBlock[:], variableBlock...))
+		block := ff1.pseudoRandomFunction(append(ff1.fixedBlock[:], variableBlock...))
 
-		byteString := make([]byte, int(math.Ceil(float64(ff1.adjustedByteStringLength)/16.0))*16)
-		copy(byteString[0:16], block)
-		mask := make([]byte, 2)
-		for blockIndex := 1; blockIndex*16 < len(block); blockIndex++ {
-			binary.BigEndian.PutUint16(mask, uint16(blockIndex))
-			ff1.cipher.Encrypt(byteString[blockIndex*16:(blockIndex+1)*16], xorBytes(block, mask))
-		}
-		adjustedByteString := byteString[0:ff1.adjustedByteStringLength]
-		adjustedByteStringNumber := big.NewInt(0)
-		adjustedByteStringNumber.SetBytes(adjustedByteString)
+		adjustedByteStringNumber := ff1.calculateAdjustedByteStringNumber(block)
 
-		modifier := ff1.secondHalfLength
+		resultStringLength := ff1.secondHalfLength
 		if round%2 == 0 {
-			modifier = ff1.firstHalfLength
+			resultStringLength = ff1.firstHalfLength
 		}
 
 		secondHalfNumber, err := strconv.ParseInt(ff1.secondHalf, ff1.radix, 64)
@@ -201,37 +145,25 @@ func (ff1 *FF1) Decrypt(message string, tweak []byte) (plaintext string, err err
 			return message, err
 		}
 
-		resultingNumber := big.NewInt(secondHalfNumber)
-		resultingNumber.Sub(big.NewInt(secondHalfNumber), adjustedByteStringNumber)
-		resultingNumber.Mod(resultingNumber, big.NewInt(int64(math.Pow(float64(ff1.radix), float64(modifier)))))
+		resultNumber := big.NewInt(0).Sub(big.NewInt(secondHalfNumber), adjustedByteStringNumber)
+		resultNumber.Mod(resultNumber, big.NewInt(int64(math.Pow(float64(ff1.radix), float64(resultStringLength)))))
 
-		resultingString := strconv.FormatUint(resultingNumber.Uint64(), ff1.radix)
-		if len(resultingString) < modifier {
-			padding := strings.Repeat("0", modifier-len(resultingString))
-			resultingString = padding + resultingString
-		}
+		resultString := zeroLeftPad(strconv.FormatUint(resultNumber.Uint64(), ff1.radix), resultStringLength)
 		ff1.secondHalf = ff1.firstHalf
-		ff1.firstHalf = resultingString
-
-		fmt.Println("Variable Block (Q): ", variableBlock)
-		fmt.Println("Block (R): ", block)
-		fmt.Printf("Byte String (S): %x\n", adjustedByteString)
-		fmt.Println("Byte String as Number (y): ", adjustedByteStringNumber)
-		fmt.Println("Modifier (m): ", modifier)
-		fmt.Println("Resulting Number (c): ", resultingNumber)
-		fmt.Println("Resulting String (C): ", resultingString)
-		fmt.Println("New First Half(A): ", ff1.firstHalf)
-		fmt.Println("New Second Half(B): ", ff1.secondHalf)
-
-		fmt.Println()
+		ff1.firstHalf = resultString
 	}
 
 	plaintext = ff1.firstHalf + ff1.secondHalf
-
-	fmt.Println("Decrypted Message (A || B): ", plaintext)
-	fmt.Println()
-
 	return plaintext, nil
+}
+
+func zeroLeftPad(input string, totalLength int) (output string) {
+	length := len(input)
+	output = input
+	if length < totalLength {
+		output = strings.Repeat("0", totalLength-length) + output
+	}
+	return output
 }
 
 func xorBytes(a, b []byte) (result []byte) {
--- fpe_test.go
+++ fpe_test.go
@@ -9,141 +9,165 @@ import (
 
 func TestFF1Encrypt(t *testing.T) {
 	t.Log("Testing FF1 encryption (case 1)... ")
-	key, err := hex.DecodeString("2B7E151628AED2A6ABF7158809CF4F3C")
-	CheckError(err)
-	cipher, err := aes.NewCipher(key)
-	CheckError(err)
-
-	ff1 := NewFF1(cipher, 10, 10, 10, 0)
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3C", 10, 1, 20, 16)
 	msg, err := ff1.Encrypt("0123456789", []byte{})
-	CheckError(err)
-
-	expectedResult := "2433477484"
-	if msg != expectedResult {
-		t.Errorf("Expected result of \"%s\", but it was \"%s\" instead.", expectedResult, msg)
-	}
+	checkError(err)
+	checkExpectedResult(t, "2433477484", msg)
 }
 
 func TestFF1Decrypt(t *testing.T) {
 	t.Log("Testing FF1 decryption (case 1)...")
-	key, err := hex.DecodeString("2B7E151628AED2A6ABF7158809CF4F3C")
-	CheckError(err)
-	cipher, err := aes.NewCipher(key)
-	CheckError(err)
-
-	ff1 := NewFF1(cipher, 10, 10, 10, 0)
-	plaintext, err := ff1.Decrypt("2433477484", []byte{0})
-	CheckError(err)
-
-	expectedResult := "0123456789"
-	if plaintext != expectedResult {
-		t.Errorf("Expected result of \"%s\", but it was \"%s\" instead.", expectedResult, plaintext)
-	}
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3C", 10, 1, 20, 16)
+	plaintext, err := ff1.Decrypt("2433477484", []byte{})
+	checkError(err)
+	checkExpectedResult(t, "0123456789", plaintext)
 }
 
 func TestFF1Encrypt2(t *testing.T) {
 	t.Log("Testing FF1 encryption (case 2)... ")
-	key, err := hex.DecodeString("2B7E151628AED2A6ABF7158809CF4F3C")
-	CheckError(err)
-	cipher, err := aes.NewCipher(key)
-	CheckError(err)
-
-	ff1 := NewFF1(cipher, 10, 10, 10, 10)
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3C", 10, 1, 20, 16)
 	msg, err := ff1.Encrypt("0123456789", []byte{0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30})
-	CheckError(err)
-
-	expectedResult := "6124200773"
-	if msg != expectedResult {
-		t.Errorf("Expected result of \"%s\", but it was \"%s\" instead.", expectedResult, msg)
-	}
+	checkError(err)
+	checkExpectedResult(t, "6124200773", msg)
 }
 
 func TestFF1Decrypt2(t *testing.T) {
 	t.Log("Testing FF1 decryption (case 2)...")
-	key, err := hex.DecodeString("2B7E151628AED2A6ABF7158809CF4F3C")
-	CheckError(err)
-	cipher, err := aes.NewCipher(key)
-	CheckError(err)
-
-	ff1 := NewFF1(cipher, 10, 10, 10, 10)
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3C", 10, 1, 20, 16)
 	plaintext, err := ff1.Decrypt("6124200773", []byte{0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30})
-	CheckError(err)
-
-	expectedResult := "0123456789"
-	if plaintext != expectedResult {
-		t.Errorf("Expected result of \"%s\", but it was \"%s\" instead.", expectedResult, plaintext)
-	}
+	checkError(err)
+	checkExpectedResult(t, "0123456789", plaintext)
 }
 
 func TestFF1Encrypt3(t *testing.T) {
 	t.Log("Testing FF1 encryption (case 3)... ")
-	key, err := hex.DecodeString("2B7E151628AED2A6ABF7158809CF4F3C")
-	CheckError(err)
-	cipher, err := aes.NewCipher(key)
-	CheckError(err)
-
-	ff1 := NewFF1(cipher, 36, 1, 20, 11)
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3C", 36, 1, 20, 16)
 	msg, err := ff1.Encrypt("0123456789abcdefghi", []byte{0x37, 0x37, 0x37, 0x37, 0x70, 0x71, 0x72, 0x73, 0x37, 0x37, 0x37})
-	CheckError(err)
-
-	expectedResult := "a9tv40mll9kdu509eum"
-	if msg != expectedResult {
-		t.Errorf("Expected result of \"%s\", but it was \"%s\" instead.", expectedResult, msg)
-	}
+	checkError(err)
+	checkExpectedResult(t, "a9tv40mll9kdu509eum", msg)
 }
 
 func TestFF1Decrypt3(t *testing.T) {
 	t.Log("Testing FF1 decryption (case 3)... ")
-	key, err := hex.DecodeString("2B7E151628AED2A6ABF7158809CF4F3C")
-	CheckError(err)
-	cipher, err := aes.NewCipher(key)
-	CheckError(err)
-
-	ff1 := NewFF1(cipher, 36, 1, 20, 11)
-	msg, err := ff1.Decrypt("a9tv40mll9kdu509eum", []byte{0x37, 0x37, 0x37, 0x37, 0x70, 0x71, 0x72, 0x73, 0x37, 0x37, 0x37})
-	CheckError(err)
-
-	expectedResult := "0123456789abcdefghi"
-	if msg != expectedResult {
-		t.Errorf("Expected result of \"%s\", but it was \"%s\" instead.", expectedResult, msg)
-	}
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3C", 36, 1, 20, 16)
+	plaintext, err := ff1.Decrypt("a9tv40mll9kdu509eum", []byte{0x37, 0x37, 0x37, 0x37, 0x70, 0x71, 0x72, 0x73, 0x37, 0x37, 0x37})
+	checkError(err)
+	checkExpectedResult(t, "0123456789abcdefghi", plaintext)
 }
 
 func TestFF1Encrypt4(t *testing.T) {
 	t.Log("Testing FF1 encryption (case 4)... ")
-	key, err := hex.DecodeString("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F")
-	CheckError(err)
-	cipher, err := aes.NewCipher(key)
-	CheckError(err)
-
-	ff1 := NewFF1(cipher, 10, 1, 20, 0)
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F", 10, 1, 20, 16)
 	msg, err := ff1.Encrypt("0123456789", []byte{})
-	CheckError(err)
-
-	expectedResult := "2830668132"
-	if msg != expectedResult {
-		t.Errorf("Expected result of \"%s\", but it was \"%s\" instead.", expectedResult, msg)
-	}
+	checkError(err)
+	checkExpectedResult(t, "2830668132", msg)
 }
 
 func TestFF1Decrypt4(t *testing.T) {
 	t.Log("Testing FF1 decryption (case 4)... ")
-	key, err := hex.DecodeString("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F")
-	CheckError(err)
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F", 10, 1, 20, 16)
+	plaintext, err := ff1.Decrypt("2830668132", []byte{})
+	checkError(err)
+	checkExpectedResult(t, "0123456789", plaintext)
+}
+
+func TestFF1Encrypt5(t *testing.T) {
+	t.Log("Testing FF1 encryption (case 5)... ")
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F", 10, 1, 20, 16)
+	msg, err := ff1.Encrypt("0123456789", []byte{0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30})
+	checkError(err)
+	checkExpectedResult(t, "2496655549", msg)
+}
+
+func TestFF1Decrypt5(t *testing.T) {
+	t.Log("Testing FF1 decryption (case 5)... ")
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F", 10, 1, 20, 16)
+	plaintext, err := ff1.Decrypt("2496655549", []byte{0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30})
+	checkError(err)
+	checkExpectedResult(t, "0123456789", plaintext)
+}
+
+func TestFF1Encrypt6(t *testing.T) {
+	t.Log("Testing FF1 encryption (case 6)... ")
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F", 36, 1, 20, 16)
+	msg, err := ff1.Encrypt("0123456789abcdefghi", []byte{0x37, 0x37, 0x37, 0x37, 0x70, 0x71, 0x72, 0x73, 0x37, 0x37, 0x37})
+	checkError(err)
+	checkExpectedResult(t, "xbj3kv35jrawxv32ysr", msg)
+}
+
+func TestFF1Decrypt6(t *testing.T) {
+	t.Log("Testing FF1 decryption (case 6)... ")
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F", 36, 1, 20, 16)
+	plaintext, err := ff1.Decrypt("xbj3kv35jrawxv32ysr", []byte{0x37, 0x37, 0x37, 0x37, 0x70, 0x71, 0x72, 0x73, 0x37, 0x37, 0x37})
+	checkError(err)
+	checkExpectedResult(t, "0123456789abcdefghi", plaintext)
+}
+
+func TestFF1Encrypt7(t *testing.T) {
+	t.Log("Testing FF1 encryption (case 7)... ")
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F7F036D6F04FC6A94", 10, 1, 20, 16)
+	msg, err := ff1.Encrypt("0123456789", []byte{})
+	checkError(err)
+	checkExpectedResult(t, "6657667009", msg)
+}
+
+func TestFF1Decrypt7(t *testing.T) {
+	t.Log("Testing FF1 decryption (case 7)... ")
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F7F036D6F04FC6A94", 10, 1, 20, 16)
+	plaintext, err := ff1.Decrypt("6657667009", []byte{})
+	checkError(err)
+	checkExpectedResult(t, "0123456789", plaintext)
+}
+
+func TestFF1Encrypt8(t *testing.T) {
+	t.Log("Testing FF1 encryption (case 8)... ")
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F7F036D6F04FC6A94", 10, 1, 20, 16)
+	msg, err := ff1.Encrypt("0123456789", []byte{0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30})
+	checkError(err)
+	checkExpectedResult(t, "1001623463", msg)
+}
+
+func TestFF1Decrypt8(t *testing.T) {
+	t.Log("Testing FF1 decryption (case 8)... ")
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F7F036D6F04FC6A94", 10, 1, 20, 16)
+	plaintext, err := ff1.Decrypt("1001623463", []byte{0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30})
+	checkError(err)
+	checkExpectedResult(t, "0123456789", plaintext)
+}
+
+func TestFF1Encrypt9(t *testing.T) {
+	t.Log("Testing FF1 encryption (case 9)... ")
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F7F036D6F04FC6A94", 36, 1, 20, 16)
+	msg, err := ff1.Encrypt("0123456789abcdefghi", []byte{0x37, 0x37, 0x37, 0x37, 0x70, 0x71, 0x72, 0x73, 0x37, 0x37, 0x37})
+	checkError(err)
+	checkExpectedResult(t, "xs8a0azh2avyalyzuwd", msg)
+}
+
+func TestFF1Decrypt9(t *testing.T) {
+	t.Log("Testing FF1 decryption (case 9)... ")
+	ff1 := setupFF1("2B7E151628AED2A6ABF7158809CF4F3CEF4359D8D580AA4F7F036D6F04FC6A94", 36, 1, 20, 16)
+	plaintext, err := ff1.Decrypt("xs8a0azh2avyalyzuwd", []byte{0x37, 0x37, 0x37, 0x37, 0x70, 0x71, 0x72, 0x73, 0x37, 0x37, 0x37})
+	checkError(err)
+	checkExpectedResult(t, "0123456789abcdefghi", plaintext)
+}
+
+func setupFF1(keyString string, radix, minMessageLength, maxMessageLength, maxTweakLength int) (ff1 FF1) {
+	key, err := hex.DecodeString(keyString)
+	checkError(err)
 	cipher, err := aes.NewCipher(key)
-	CheckError(err)
+	checkError(err)
 
-	ff1 := NewFF1(cipher, 10, 1, 20, 0)
-	msg, err := ff1.Decrypt("2830668132", []byte{})
-	CheckError(err)
+	ff1 = NewFF1(cipher, radix, minMessageLength, maxMessageLength, maxTweakLength)
+	return ff1
+}
 
-	expectedResult := "0123456789"
-	if msg != expectedResult {
-		t.Errorf("Expected result of \"%s\", but it was \"%s\" instead.", expectedResult, msg)
+func checkExpectedResult(t *testing.T, expected, actual string) {
+	if expected != actual {
+		t.Errorf("Expected result of \"%s\", but it was \"%s\" instead.", expected, actual)
 	}
 }
 
-func CheckError(err error) {
+func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
